778ea030e6e93b75ef896cb4da3b12616ba00452
rabia-creator <ibeleive.rab@gmail.com>
2020-10-31 06:22:38 -0700

    change in msg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import React from 'react' 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    hello world  
+    <h1>hello world from Rabia</h1>  
     </div>
   );
 }
